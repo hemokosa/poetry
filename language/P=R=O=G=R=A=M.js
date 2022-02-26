4a586164c190e87748fdd3414edc838f9eee6195
Akihiro Kubota <akihiro.kubota@nifty.com>
2022-02-26 22:03:29 +0900

    update

---
+++ language/P=R=O=G=R=A=M.js
@@ -0,0 +1,268 @@
+
+PSEUDOPSEUDOHYPOPARATHYROIDISM 30 擬似副甲状腺機能低下症○
+FLOCCINAUCINIHILIPILIFICATION 29 無価値○
+ANTIDISESTABLISHMENTARIANISM 28 反体制派○
+PSYCHONEUROENDOCRINOLOGICAL 27 精神神経内分泌学○
+
+
+
+RADIOIMMUNOELECTROPHORESIS 26 放射免疫電気泳動○
+THYROPARATHYROIDECTOMIZED 25 涙腺切除術○
+SCHIZOSACCHAROMYCETACEAE 24 分裂酵母○
+ELECTROENCEPHALOGRAPHIC 23 脳波計○
+COUNTERREVOLUTIONARIES 22 反革命派○
+INCOMPREHENSIBILITIES 21 理解できないこと○
+UNCHARACTERISTICALLY 20 特徴のない○
+XENOTRANSPLANTATION 19 異種移植
+FORESIGHTFORTNIGHT 18 先見の明
+VIDEOCONFERENCING 17 ビデオ会議
+ANATHEMATISATION 16 麻酔
+BIOAVAILABILITY 15
+QUANTIFICATION
+DESEGREGATION
+HIEROGLYPHIC 12
+GEOPOLITICS
+PARASITIZE
+JELLYFISH
+LANGUAGE
+KEYHOLE
+YELLOW
+NAKED
+WILD
+MAP
+ON
+Z
+
+
+RAT
+TBD
+SCI
+EDU
+CEO
+IFS
+UGH
+XHV
+FIG
+VJC
+AKA
+BLA
+QML
+DNA
+HOT
+GPU
+PQQ
+JRE
+LSD
+KTU
+YUK
+NVT
+WWW
+MXM
+OYO
+ZZT
+
+
+ART
+BTW
+CSA
+DEG
+ECO
+FIG
+GUM
+HXT
+IFD
+JVM
+KAW
+LBP
+MQL
+NDA
+OHM
+PGP
+QPU
+RJE
+SLY
+TKO
+UYL
+VNC
+WWI
+XML
+YOU
+ZZZ
+
+
+ANT
+BUG
+CAW
+DYE
+ELF
+FOG
+GOD
+HAY
+IVY
+JUG
+KIN
+LOS
+MOB
+NOH
+OPT
+PEG
+QED
+RAM
+SEA
+TAX
+UFO
+VIA
+WOK
+XML
+YEN
+ZEN
+
+naked
+equilibrium
+
+Interdisciplinary　17
+
+Uncopyrightable 15 重複なし
+Inconsequential 15
+Dermatoglyphics 15 重複なし
+Hypothetically　14
+Surreptitious 13
+
+
+autophosphorylation｜自己リン酸化
+bacteriological｜細菌学
+bioavailability｜バイオアベイラビリティ
+compartmentalization｜区画化
+counterrevolutionary｜反革命的
+disproportionately｜不釣り合いに
+
+
+spectrophotometrically｜分光光度法　22
+immunohistochemically｜免疫組織化学的に　21
+uncharacteristically｜特徴のない 20
+
+Chlorobenzylidenemalononitrile 30 クロロベンジリデンマロノニトリル
+
+pseudopseudohypoparathyroidism　30 擬似副甲状腺機能低下症○
+
+floccinaucinihilipilification 29 無価値○
+
+Antidisestablishmentarianism　28 反体制派○
+
+Spectrophotofluorometrically　28
+Honorificabilitudinitatibus　名誉を達成することができる状態　27 with honorableness.
+
+electroencephalographically　27 脳波学
+
+Psychoneuroendocrinological　27 精神神経内分泌学○
+
+Radioimmunoelectrophoresis　26 放射免疫電気泳動○
+
+Pneumoencephalographically 26 気性脳症
+
+Thyroparathyroidectomized 25 涙腺切除術○
+
+Psychophysicotherapeutics 25 精神生理療法
+
+Immunoelectrophoretically 25 免疫電気泳動
+
+Uvulopalatopharyngoplasty　25 口蓋垂口蓋咽頭形成術
+
+Aldiborontiphoscophornio　24
+
+
+Schizosaccharomycetaceae 24 分裂酵母○
+laryngotracheobronchitis 24 喉頭・気管・気管支炎
+hyperbetalipoproteinemia 24 高ベタリン酸血症
+
+Dichlorodifluoromethane　23
+ELECTROENCEPHALOGRAPHIC 23 脳波計○
+
+Counterrevolutionaries 22 反革命派○
+
+Otorhinolaryngological　22 耳鼻咽喉科
+
+Incomprehensibilities　21 理解できないこと○
+Uncharacteristically 20 特徴のない○
+
+
+
+Interdisciplinary　17
+
+Uncopyrightable 15 重複なし
+Inconsequential 15
+Dermatoglyphics 15 重複なし
+Hypothetically　14
+Surreptitious 13
+
+
+function setup() {
+    createCanvas(600, 600, WEBGL); frameRate(0.1);
+    $ = () => random(-200, 200);
+    _ = (a, b, c) => curveVertex(a, b, c);
+}
+
+function draw() {
+
+    R = A = D = I = O = I = M = M = U = N = O = E = L = E = C = T = R = O = P = H = O = R = E = S = I = S = $();
+    T = H = Y = R = O = P = A = R = A = T = H = Y = R = O = I = D = E = C = T = O = M = I = Z = E = D = $();
+    S = C = H = I = Z = O = S = A = C = C = H = A = R = O = M = Y = C = E = T = A = C = E = A = E = $();
+    E = L = E = C = T = R = O = E = N = C = E = P = H = A = L = O = G = R = A = P = H = I = C = $();
+    C = O = U = N = T = E = R = R = E = V = O = L = U = T = I = O = N = A = R = I = E = S = $();
+    I = N = C = O = M = P = R = E = H = E = N = S = I = B = I = L = I = T = I = E = S = $();
+    U = N = C = H = A = R = A = C = T = E = R = I = S = T = I = C = A = L = L = Y = $();
+    X = E = N = O = T = R = A = N = S = P = L = A = N = T = A = T = I = O = N = $();
+    F = O = R = E = S = I = G = H = T = F = O = R = T = N = I = G = H = T = $();
+    V = I = D = E = O = C = O = N = F = E = R = E = N = C = I = N = G = $();
+    A = N = A = T = H = E = M = A = T = I = S = A = T = I = O = N = $();
+    B = I = O = A = V = A = I = L = A = B = I = L = I = T = Y = $();
+    Q = U = A = N = T = I = F = I = C = A = T = I = O = N = $();
+    D = E = S = E = G = R = E = G = A = T = I = O = N = $();
+    H = I = E = R = O = G = L = Y = P = H = I = C = $();
+    G = E = O = P = O = L = I = T = I = C = S = $();
+    P = A = R = A = S = I = T = I = Z = E = $();
+    J = E = L = L = Y = F = I = S = H = $();
+    L = A = N = G = U = A = G = E = $();
+    K = E = Y = H = O = L = E = $();
+    Y = E = L = L = O = W = $();
+    N = A = K = E = D = $();
+    W = I = L = D = $();
+    M = A = P = $();
+    O = N = $();
+    Z = $();
+
+    clear();
+    beginShape();
+
+    _(A, R, T);
+    _(B, T, W);
+    _(C, S, A);
+    _(D, E, G);
+    _(E, C, O);
+    _(F, I, G);
+    _(G, U, M);
+    _(H, X, T);
+    _(I, F, D);
+    _(J, V, M);
+    _(K, A, W);
+    _(L, B, P);
+    _(M, Q, L);
+    _(N, D, A);
+    _(O, H, M);
+    _(P, G, P);
+    _(Q, P, U);
+    _(R, J, E);
+    _(S, L, Y);
+    _(T, K, O);
+    _(U, Y, L);
+    _(V, N, C);
+    _(W, W, I);
+    _(X, M, L);
+    _(Y, O, U);
+    _(Z, Z, Z);
+
+    endShape();
+}
+
+saveFrames(String(Z), 'png', 10, 0.1);
+saveCanvas(String(Z));
